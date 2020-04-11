504ca6dea5f5308acf2de4f3ac813fbde567728e
joshhait <44282219+joshhait@users.noreply.github.com>
2020-04-11 14:06:58 -0700

    create skeleton menu for query running

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import datetime
 from datetime import date
 import getpass
 import sys
+
 def create_tables(conn):
     connection = conn
     try:
@@ -12,6 +13,7 @@ def create_tables(conn):
         connection.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+
 def drop_tables(conn):
         connection = conn
         try:
@@ -20,6 +22,7 @@ def drop_tables(conn):
             connection.commit()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
+
 def insert_data(conn):
             connection = conn
             try:
@@ -33,6 +36,87 @@ def insert_data(conn):
                 connection.commit()
             except (Exception, psycopg2.DatabaseError) as error:
                 print(error)
+
+def run_queries(conn):
+    inp = None
+    while (inp != 'q'):
+        print("\nSelect a query to run (or q to return to the previous menu): ")
+        print(  "[1] Find competitions open during a specific month \
+                \n[2] Find competitions by area and PI \
+                \n[3] Largest Proposal for an area \
+                \n[4] Proposals submitted before a date awarded the largest amount of money \
+                \n[5] Average discrepancy between requested amount and awarded ammount for an area \
+                \n[6] Assign reviewers to a grant application \
+                \n[7] Check if a meeting room is available \
+                \n[q] Return")
+        inp = input("Enter a menu option: ")
+        print(inp)
+        if(inp == '1'):
+            print("Running query 1...")
+            run_query1(conn)
+        elif(inp == '2'):
+            print("Running query 2...")
+            run_query1(conn)
+        elif(inp == '3'):
+            print("Running query 3...")
+            run_query2(conn)
+        elif(inp == '4'):
+            print("Running query 4...")
+            run_query4(conn)
+        elif(inp == '5'):
+            print("Running query 5...")
+            run_query5(conn)
+        elif(inp == '6'):
+            print("Running query 6...")
+            run_query6(conn)
+        elif(inp == '7'):
+            print("Running query 7...")
+            run_query7(conn)
+        elif(inp == 'q' or inp == 'Q'):
+            print("Returning to previous menu...\n")
+
+def run_query1(conn):
+    ### Insert code for query 1 here ###
+
+    ### end of query 1 code ###
+    pass
+
+def run_query2(conn):
+    ### Insert code for query 2 here ###
+
+    ### end of query 2 code ###
+    pass
+
+def run_query3(conn):
+    ### Insert code for query 3 here ###
+
+    ### end of query 3 code ###
+    pass
+
+def run_query4(conn):
+    ### Insert code for query 4 here ###
+
+    ### end of query 4 code ###
+    pass
+
+def run_query5(conn):
+    ### Insert code for query 5 here ###
+
+    ### end of query 5 code ###
+    pass
+
+def run_query6(conn):
+    ### Insert code for query 6 here ###
+
+    ### end of query 6 code ###
+    pass
+
+def run_query7(conn):
+    ### Insert code for query 7 here ###
+
+    ### end of query 7 code ###
+    pass
+
 def main():
     if not sys.stdin.isatty():
         print("Use 'winpty python main.py' instead")
@@ -59,7 +143,8 @@ def main():
         print("You are connected to - ", record,"\n")
         inp = None
         while(inp != 'q'):
-            print("[1] to create Tables \n[2] to Drop Tables\n[3] to insert data\n[q] to exit")
+            print("What would you like to do? Enter: ")
+            print("[1] to Create all Tables \n[2] to Drop all Tables\n[3] to Insert all Data\n[4] to Run Queries \n[q] to exit")
             inp = input("Select a Command: ")
             print(inp)
             if(inp == '1'):
@@ -68,6 +153,8 @@ def main():
                 drop_tables(connection)
             elif(inp == '3'):
                 insert_data(connection)
+            elif(inp == '4'):
+                run_queries(connection)
             elif(inp == 'q' or input == 'Q'):
                 print("goodbye :(")
             else:

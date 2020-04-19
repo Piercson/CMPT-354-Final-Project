59ef2d25cacdce22096d44eb652ead0436bbc4ae
joshhait <joshuahait@gmail.com>
2020-04-19 15:37:16 -0700

    update to new schema

--- insert_proposal.sql
+++ insert_proposal.sql
@@ -1,4 +1,4 @@
 INSERT INTO proposal VALUES
-(DEFAULT,1,1,'awarded',3600),
-(DEFAULT,3,4,'submitted',0),
-(DEFAULT,5,2,'denied',0);
+(DEFAULT,1,1,'awarded',36000, 30000),
+(DEFAULT,3,4,'submitted',25000, 0),
+(DEFAULT,5,2,'denied',30000, 0);

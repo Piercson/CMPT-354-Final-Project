5321e38fb64949cac85f34ff75a1f32db93afdf0
Ryan Kiew <rkiew@sfu.ca>
2020-04-20 18:57:55 -0700

    fixed Q4

--- main.py
+++ main.py
@@ -180,11 +180,24 @@ def run_query4(conn):
     cur = conn.cursor()
     Q4_DATE = input("Please enter the date (YYYY-MM-DD): ")
 
-    cur.execute(
-    "SELECT narrowed1.id FROM (SELECT p1.* \
-                                FROM (SELECT * \
-                                    FROM call c1 WHERE c1.deadline < %s) as c2 JOIN proposal p1 ON p1.callid = c2.id) as narrowed1 INNER JOIN (SELECT MAX(p3.amount) as max_req \
-        FROM (SELECT * FROM call c3 WHERE c3.deadline < %s) as c4 JOIN proposal p3 ON p3.callid = c4.id) as narrowed2 ON narrowed1.requestedamount = narrowed2.max_req;", (Q4_DATE, Q4_DATE))
+    query = "SELECT narrowed1.id, narrowed1.requestedamount \
+            FROM ( \
+                SELECT p1.* \
+                FROM ( \
+                    SELECT * \
+                    FROM call c1 \
+                    WHERE c1.deadline < %s) as c2 \
+                JOIN proposal p1 ON p1.callid = c2.id) as narrowed1 \
+            INNER JOIN ( \
+                SELECT MAX(p3.requestedamount) as max_req \
+                FROM ( \
+                    SELECT * \
+                    FROM call c3 \
+                    WHERE c3.deadline < %s) as c4 \
+                JOIN proposal p3 ON p3.callid = c4.id) as narrowed2 \
+            ON narrowed1.requestedamount = narrowed2.max_req;"
+    data = (Q4_DATE, Q4_DATE)
+    cur.execute(query,data)
     results = cur.fetchall()
     for row in results:
         print(row)
